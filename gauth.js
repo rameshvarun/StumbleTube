9fb83d7119f0ad4c89ccd56ebaef175b330c3150
Varun Ramesh <varun.ramesh@aol.com>
2013-10-11 22:11:10 -0700

    Fixed some mistakes

--- gauth.js
+++ gauth.js
@@ -8,11 +8,11 @@ exports.CLIENT_ID = CLIENT_ID;
 var CLIENT_SECRET = 'yH4Azb_hwLlawzKtbO_1y2tO';
 var REDIRECT_URL = 'http://stumbletube.herokuapp.com/oauth2callback';
 
-exports.oauth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+oauth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
 
-exports.scopes = "https://www.googleapis.com/auth/plus.me https://www.googleapis.com/auth/youtube";
+var scopes = "https://www.googleapis.com/auth/plus.me https://www.googleapis.com/auth/youtube";
 
 exports.loginurl = oauth2Client.generateAuthUrl({
 	  access_type: 'offline',
-	  scope: gauth.scopes
+	  scope: scopes
 	});
